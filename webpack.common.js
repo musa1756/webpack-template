ccbff94579b5225461881047798f9bb7ed1a2788
Musa <musa1756@proton.me>
2025-12-26 00:57:22 +0300

    default

--- webpack.common.js
+++ webpack.common.js
@@ -2,31 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-        ],
-    },
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,11 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map', // Показывает ошибки в твоих файлах, а не в бандле
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true,
+  },
 });
